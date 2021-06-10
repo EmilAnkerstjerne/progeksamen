261bf4182d21fc250252db66364f27632da4e6d9
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-06-10 10:07:43 +0200

    first commit

---
+++ src/main/java/com/example/progeksamen/ProgeksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.progeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgeksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgeksamenApplication.class, args);
+    }
+
+}
